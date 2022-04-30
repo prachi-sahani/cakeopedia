24d2ebd38b46253b9348f7224681a8cf2ea93886
Prachi Sahani <prachirsahani97@gmail.com>
2022-04-30 21:59:34 +0530

    feat - included side menu and added grid and list view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import { Link, useLocation } from "react-router-dom";
+import { useAuth } from "../context/authorization-context";
+import { useMessageHandling } from "../context/message-handling-context";
 import "../stylesheets/navbar.css";
 
 export function Navbar() {
   const location = useLocation();
+  const { authToken, logout } = useAuth();
+  const { setShowSidenav, gridView, setGridView } = useMessageHandling();
+
   return (
     <nav className="navbar">
+      <button
+        className="btn-icon btn-outline-primary material-icons-outlined"
+        onClick={() => setShowSidenav((value) => !value)}
+      >
+        menu
+      </button>
       <Link to="/">
         <img className="logo" src="assets/logo.png" alt="logo" />
       </Link>
@@ -23,15 +34,27 @@ export function Navbar() {
         location.pathname.includes("signup")
       ) && (
         <div className="navbar-action">
-          <button className="btn-icon btn-outline-primary material-icons">
-            view_list
-          </button>
-          <Link
-            to="/login"
-            className="link btn-icon btn-outline-primary material-icons"
+          <button
+            className="btn-icon btn-outline-primary material-icons-outlined view-toggle-icon"
+            onClick={() => setGridView((value) => !value)}
           >
-            login
-          </Link>
+            {gridView ? "view_agenda" : "grid_view"}
+          </button>
+          {authToken ? (
+            <button
+              className="link btn-icon btn-outline-primary material-icons"
+              onClick={logout}
+            >
+              logout
+            </button>
+          ) : (
+            <Link
+              to="/login"
+              className="link btn-icon btn-outline-primary material-icons"
+            >
+              login
+            </Link>
+          )}
         </div>
       )}
     </nav>
---
+++ src/utilities/sidenav-items.js
@@ -0,0 +1,37 @@
+export const sidenavItemList = [
+  {
+    title: "Notes",
+    iconName: "description",
+    route: "/notes",
+  },
+  {
+    title: "Personal",
+    iconName: "label",
+    route: "/",
+  },
+  {
+    title: "Work",
+    iconName: "label",
+    route: "/",
+  },
+  {
+    title: "Inspiration",
+    iconName: "label",
+    route: "/",
+  },
+  {
+    title: "Edit Label",
+    iconName: "new_label",
+    route: "/",
+  },
+  {
+    title: "Archive",
+    iconName: "archive",
+    route: "/",
+  },
+  {
+    title: "Trash",
+    iconName: "delete",
+    route: "/",
+  },
+];
