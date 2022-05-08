a85edc7dcdc54aa2a223e78fe3f065ad4e8eca9d
Prachi Sahani <prachirsahani97@gmail.com>
2022-05-08 10:59:02 +0530

    refactor - added RequireAuth for private routes

---
+++ src/components/RequireAuth.jsx
@@ -0,0 +1,12 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../context/authorization-context";
+
+export function RequireAuth({ children }) {
+  const location = useLocation();
+  const { authToken } = useAuth();
+  return authToken ? (
+    children
+  ) : (
+    <Navigate to="/login" replace state={{ from: location }} />
+  );
+}
